30727a19af2c8a25461cb45a119351ecf4746e7d
sahanes <sahanes_2000@yahoo.com>
2017-05-11 22:20:47 -0400

    Initial Commit

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stack
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Stack
+{
+    public class Stack
+    {
+
+
+        private List<object> _stack;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Cannot add null object to stack!");
+            }
+            else
+            {
+                _stack.Add(obj);
+            }
+
+        }
+
+        public object Pop()
+        {
+            int count = _stack.Count;
+            object output;
+
+            if (count > 0)
+            {
+                output = _stack[count - 1];
+                _stack.RemoveAt(count - 1);
+                return output;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
